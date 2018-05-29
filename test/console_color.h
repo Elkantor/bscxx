b101eb49ca6dec3701146456b53bac448507109e
Elkantor <vic03@live.fr>
2018-05-29 11:34:24 +0200

    Add color output for unix systems

--- test/console_color.h
+++ test/console_color.h
@@ -1,46 +1,71 @@
-// ConsoleColor.h
-
 #pragma once
+
 #include <iostream>
-#include <windows.h>
+#if defined(_WIN32)
+    #include <windows.h>
+#else
+    #include <unistd.h>
+#endif
+
+
 
 inline std::ostream& blue(std::ostream &s)
 {
-    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE); 
-    SetConsoleTextAttribute(hStdout, FOREGROUND_BLUE
-              |FOREGROUND_GREEN|FOREGROUND_INTENSITY);
+    #if defined(_WIN32)
+        HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE); 
+        SetConsoleTextAttribute(hStdout, FOREGROUND_BLUE
+                |FOREGROUND_GREEN|FOREGROUND_INTENSITY);
+    #else 
+        s << "\033[0;36m";
+    #endif
     return s;
 }
 
 inline std::ostream& red(std::ostream &s)
 {
-    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE); 
-    SetConsoleTextAttribute(hStdout, 
-                FOREGROUND_RED|FOREGROUND_INTENSITY);
+    #if defined(_WIN32)
+        HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE); 
+        SetConsoleTextAttribute(hStdout, 
+                    FOREGROUND_RED|FOREGROUND_INTENSITY);
+    #else 
+        s << "\033[0;31m";
+    #endif
     return s;
 }
 
 inline std::ostream& green(std::ostream &s)
 {
-    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE); 
-    SetConsoleTextAttribute(hStdout, 
-              FOREGROUND_GREEN|FOREGROUND_INTENSITY);
+    #if defined(_WIN32)
+        HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE); 
+        SetConsoleTextAttribute(hStdout, 
+                FOREGROUND_GREEN|FOREGROUND_INTENSITY);
+    #else 
+        s << "\033[1;32m";
+    #endif
     return s;
 }
 
 inline std::ostream& yellow(std::ostream &s)
 {
-    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE); 
-    SetConsoleTextAttribute(hStdout, 
-         FOREGROUND_GREEN|FOREGROUND_RED|FOREGROUND_INTENSITY);
+    #if defined(_WIN32)
+        HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE); 
+        SetConsoleTextAttribute(hStdout, 
+            FOREGROUND_GREEN|FOREGROUND_RED|FOREGROUND_INTENSITY);
+    #else 
+        s << "\033[1;33m";
+    #endif
     return s;
 }
 
 inline std::ostream& white(std::ostream &s)
 {
-    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE); 
-    SetConsoleTextAttribute(hStdout, 
-       FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE);
+    #if defined(_WIN32)
+        HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE); 
+        SetConsoleTextAttribute(hStdout, 
+        FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE);
+    #else 
+        s << "\033[0m";
+    #endif
     return s;
 }
 
